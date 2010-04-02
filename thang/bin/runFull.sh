3f44d38630a5400043072c4fd8325adb8b5f8046
remote <minh_thang@nus.edu.sg>
2010-04-03 03:17:27 +0800

    lmthang@cte.comp.nus.edu.sg Sat Apr  3 03:17:27 SGT 2010

--- thang/bin/runFull.sh
+++ thang/bin/runFull.sh
@@ -10,11 +10,11 @@ ROOT="../../.."
 FOLD=$1
 LABEL="label-xml"
 F=3
-NAME="baseline_pc-pos-punct-num-length_xml-Loc-Align-Bold-Italic-fS-Bullet-Dd-Cell"
+NAME="baseline_pc-pos-punct-num-length_xml-Loc-Align-Bold-Italic-fS-Bullet-Dd-Cell-Para"
 echo "### $NAME"
 date
-mkdir "$NAME"
-cd "$NAME"
+mkdir "$FOLD$NAME"
+cd "$FOLD$NAME"
 echo "$ROOT/../bin/sectLabel/redo.sectLabel.pl -t \"ACL09-ACM-CHI08\" -in $ROOT/../thang/sectLabel/$LABEL -out . -n $FOLD -p 7 -iter 100 -c ../config/$NAME -f $F"
 $ROOT/../bin/sectLabel/redo.sectLabel.pl -t "ACL09-ACM-CHI08" -in $ROOT/../thang/sectLabel/$LABEL -out . -n $FOLD -p 7 -iter 100 -c ../config/$NAME -f $F
 cd ..
