135f7c9b07411e845d7db630c6044ebe2a30ac2f
Zakariyya Mughal <zaki.mughal@gmail.com>
2017-12-25 20:07:32 -0600

    Add Alien for crfpp

--- lib/ParsCit.pm
+++ lib/ParsCit.pm
@@ -1,4 +1,20 @@
 package ParsCit;
 # ABSTRACT: An open-source CRF Reference String and Logical Document Structure Parsing Package
 
+use strict;
+use warnings;
+
+use File::Spec;
+
+use parent qw(Alien::Base);
+
+sub crf_test_path {
+	my ($self) = @_;
+	File::Spec->catfile( File::Spec->rel2abs($self->dist_dir) , 'bin', 'crf_test' );
+}
+sub crf_learn_path {
+	my ($self) = @_;
+	File::Spec->catfile( File::Spec->rel2abs($self->dist_dir) , 'bin', 'crf_learn' );
+}
+
 1;
