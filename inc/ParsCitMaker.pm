f34c15419d9416b52f775614ed786cad089d9331
Zakariyya Mughal <zaki.mughal@gmail.com>
2017-12-25 20:59:38 -0600

    Update build

--- inc/ParsCitMaker.pm
+++ inc/ParsCitMaker.pm
@@ -1,6 +1,29 @@
-package inc::ParsCitMaker;
+package ParsCitMaker;
 use Moose;
 
-extends qw(Dist::Zilla::Plugin::AlienBuild);
+with 'Dist::Zilla::Role::FileMunger';
+
+use List::Util qw( first );
+
+sub munge_files {
+	my ($self) = @_;
+	my $file = first { $_->name eq 'Makefile.PL' } @{ $self->zilla->files };
+	my $content = $file->content;
+
+	$content .= <<'EOF';
+# BEGIN code inserted by ParsCitMaker
+{
+	package MY;
+	no warnings 'redefine';
+	use File::ShareDir::Install;
+	sub postamble {
+		join "\n", $abmm->mm_postamble, File::ShareDir::Install::postamble(@_);
+	}
+}
+# END code inserted by ParsCitMaker
+EOF
+
+	$file->content( $content );
+};
 
 __PACKAGE__->meta->make_immutable;
